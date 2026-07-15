b934d969b1a7d90308160f594e8377b1c477dc6e
Seusara <edroexa@gmail.com>
2026-07-15 17:16:32 -0600

    Add power and sqrt functions to math utils

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { add, multiply, divide } = require('./math');
+const { add, multiply, divide, power, sqrt } = require('./math');
 
 function greet(name) {
   return `Hello, ${name}!`;
@@ -9,6 +9,8 @@ const name = process.argv[2] || 'World';
 console.log(greet(name));
 console.log(`2 + 3 = ${add(2, 3)}`);
 console.log(`2 * 3 = ${multiply(2, 3)}`);
+console.log(`2 ^ 10 = ${power(2, 10)}`);
+console.log(`sqrt(81) = ${sqrt(81)}`);
 
 try {
   console.log(`10 / 0 = ${divide(10, 0)}`);
