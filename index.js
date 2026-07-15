dd3be8652bafaff3b86b39387ff4ffd1100b61e0
Seusara <edroexa@gmail.com>
2026-07-15 17:10:28 -0600

    Add multiply/divide to math utils and use them in index.js

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
+const { add, multiply } = require('./math');
+
 function greet(name) {
   return `Hello, ${name}!`;
 }
 
 console.log(greet('World'));
+console.log(`2 + 3 = ${add(2, 3)}`);
+console.log(`2 * 3 = ${multiply(2, 3)}`);
 
 module.exports = { greet };
